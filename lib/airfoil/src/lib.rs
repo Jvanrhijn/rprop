bdaf904931b3100dace262f691c1e22f27d01f7c
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-12-18 23:42:39 +0100

    Fully implement Airfoil::area()

--- lib/airfoil/src/lib.rs
+++ lib/airfoil/src/lib.rs
@@ -1,4 +1,7 @@
 use std::vec::Vec;
+use std::io;
+use std::io::{BufReader, BufRead};
+use std::fs::File;
 use math;
 
 pub struct Airfoil {
@@ -11,12 +14,42 @@ impl Airfoil {
         Self{x_values: xvals, y_values: yvals}
     }
     
-    pub fn from_file(fpath: &str) -> Self {
-        Self{x_values: vec![1.0], y_values: vec![1.0]}
+    pub fn from_file(fpath: &str) -> Result<Self, io::Error> {
+        let mut xvals = Vec::<f64>::new();
+        let mut yvals = Vec::<f64>::new();
+        let file = File::open(fpath)?;
+        for line in BufReader::new(&file).lines() {
+            let coordinates = line?.split_whitespace().map(|x| x.parse::<f64>()
+                .expect("Failed to parse f64")).collect::<Vec<f64>>();
+            xvals.push(coordinates[0]);
+            yvals.push(coordinates[1]);
+        }
+        Ok(Self{x_values: xvals, y_values: yvals})
     }
 
     pub fn area(&self) -> Result<f64, math::MathError> {
-        math::trapz(&self.x_values, &self.y_values)
+        let (xtop, ytop) = self.top_surface();
+        let (xbot, ybot) = self.bottom_surface();
+        let ytop_min = ytop.iter().min_by(|x, y| x.partial_cmp(y).unwrap()).unwrap();
+        let ybot_min = ybot.iter().min_by(|x, y| x.partial_cmp(y).unwrap()).unwrap();
+        let dy = ytop_min.min(*ybot_min).abs();
+        // shift airfoil up so it is entirely above x-axis
+        let ytop = ytop.iter().map(|y| y + dy).collect::<Vec<_>>();
+        let ybot = ybot.iter().map(|y| y + dy).collect::<Vec<_>>();
+        // top area must be negated since x-axis is reversed
+        let area_top = -math::trapz(&xtop, &ytop)?;
+        let area_bot = math::trapz(&xbot, &ybot)?;
+        Ok(area_top - area_bot)
+    }
+
+    fn top_surface(&self) -> (&[f64], &[f64]) {
+        let num_points = self.x_values.len();
+        (&self.x_values[..num_points/2], &self.y_values[..num_points/2])
+    }
+
+    fn bottom_surface(&self) -> (&[f64], &[f64]) {
+        let num_points = self.x_values.len();
+        (&self.x_values[num_points/2..], &self.y_values[num_points/2..])
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use math::trapz;
+use airfoil::Airfoil;
 
 fn main() {
-    println!("Hello!");
+    let foil = Airfoil::from_file("NACA2414.dat").unwrap();
+    println!("{}", foil.area().unwrap());
 }
