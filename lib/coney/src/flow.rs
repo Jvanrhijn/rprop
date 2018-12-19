ffa343e2cf6bc4f9c7174a239b505be989ff884a
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-12-19 16:10:19 +0100

    Implement tangential and axial horseshoe influence functions

--- lib/coney/src/flow.rs
+++ lib/coney/src/flow.rs
@@ -1,28 +1,24 @@
 use std::f64::consts::PI;
-use propeller::Propeller;
 use math;
 
-fn coney_u(y0: f64, y: f64, prop: &Propeller) -> f64 {
-    let z = *prop.geometry().num_blades();
+fn coney_u(y0: f64, y: f64, z: usize) -> f64 {
     let exp_factor = ((1.0 + y*y).sqrt() - (1.0 + y0*y0).sqrt()).exp();
     let frac = y0*((1.0 + y*y).sqrt() - 1.0)/(y*((1.0 + y0*y0).sqrt() - 1.0));
     (frac*exp_factor).powi(z as i32)
 }
 
-fn coney_f1(y0: f64, y: f64, prop: &Propeller) -> f64 {
-    let z = *prop.geometry().num_blades();
+fn coney_f1(y0: f64, y: f64, z: usize) -> f64 {
     let prefactor = -1.0/(2.0*(z as f64)*y0)*((1.0 + y0.powi(2))/(1.0 + y.powi(2))).powf(0.25);
-    let u = coney_u(y0, y, prop);
+    let u = coney_u(y0, y, z);
     let ufrac = 1.0/(1.0/u - 1.0);
     let first_frac = (9.0*y0*y0 + 2.0)/(1.0 + y0*y0).powf(1.5);
     let second_frac = (3.0*y*y - 2.0)/(1.0 + y*y).powf(1.5);
     prefactor*(ufrac + 1.0/(24.0*(z as f64))*(first_frac + second_frac)*(1.0 + ufrac).ln())
 }
 
-fn coney_f2(y0: f64, y: f64, prop: &Propeller) -> f64 {
-    let z = *prop.geometry().num_blades();
+fn coney_f2(y0: f64, y: f64, z: usize) -> f64 {
     let prefactor = 1.0/(2.0*(z as f64)*y0)*((1.0 + y0.powi(2))/(1.0 + y.powi(2))).powf(0.25);
-    let u = coney_u(y0, y, prop);
+    let u = coney_u(y0, y, z);
     let ufrac = 1.0/(u - 1.0);
     let first_frac = (9.0*y0*y0 + 2.0)/(1.0 + y0*y0).powf(1.5);
     let second_frac = (3.0*y*y - 2.0)/(1.0 + y*y).powf(1.5);
@@ -55,6 +51,58 @@ fn c2_jl(xf: f64, rcjm: f64, rvlp: f64) -> f64 {
     }
 }
 
-struct FlowField {
+/// axial component of velocity field induced at r = rc by z helical vortex lines
+/// originating at r = rv at pitch angle vp
+fn axial_velocity_vortex_line(rc: f64, rv: f64, vp: f64, z: usize) -> f64 {
+    let tanvp = vp.tan();
+    let y = rc/(rv*tanvp);
+    let y0 = 1.0/tanvp;
+    if rc <= rv {
+        (z as f64 / (2.0*rv*tanvp) - y*z.pow(2) as f64*coney_f1(y0, y, z)/rc)/(2.0*PI)
+    } else {
+        (-y*z.pow(2) as f64*y0/rc*coney_f2(y0, y, z)/rc)/(2.0*PI)
+    }
+}
 
+/// tangential component of velocity field induced at r = rc by z helical vortex lines
+/// originating at r = rv at pitch angle vp
+fn tangential_velocity_vortex_line(rc: f64, rv: f64, vp: f64, z: usize) -> f64 {
+    let tanvp = vp.tan();
+    let y = rc/(rv*tanvp);
+    let y0 = 1.0/tanvp;
+    if rc <= rv {
+        (z.pow(2) as f64/rc*y0*coney_f1(y0, y, z))/(2.0*PI)
+    } else {
+        (z as f64/(2.0*rc) + z.pow(2) as f64*y0*coney_f2(y0, y, z)/rc)/(2.0*PI)
+    }
+}
+
+/// Total axial velocity induced by horseshoe vortex at
+/// rv[vi], rvi[vi+1] on the control point at rc[ci]
+/// including hub image effects due to hub of radius rh, for
+/// a propeller of z blades
+pub fn axial_velocity(ci: usize, vi: usize, rc: &[f64], rv: &[f64], vpitch: &[f64], rh: f64, z: usize) -> f64 {
+    // vortex line contributions
+    let uaw1 = axial_velocity_vortex_line(rc[ci], rv[vi+1], (vpitch[vi].tan()*rc[vi]/rv[vi+1]).atan(), z);
+    let uaw2 = axial_velocity_vortex_line(rc[ci], rv[vi], (vpitch[vi].tan()*rc[vi]/rv[vi]).atan(), z);
+    // image hub contributions
+    let uawh1 = axial_velocity_vortex_line(rc[ci], rh*rh/rv[vi+1], (vpitch[vi].tan()*rc[vi]/(rh*rh/rv[vi+1])).atan(), z);
+    let uawh2 = axial_velocity_vortex_line(rc[ci], rh*rh/rv[vi], (vpitch[vi].tan()*rc[vi]/(rh*rh/rv[vi])).atan(), z);
+    // horsehoe vortex + image hub vortex
+    (uaw1 - uawh2) - (uaw2 - uawh2)
+}
+
+/// Total axial velocity induced by horseshoe vortex at
+/// rv[vi], rvi[vi+1] on the control point at rc[ci]
+/// including hub image effects due to hub of radius rh, for
+/// a propeller of z blades
+pub fn tangential_velocity(ci: usize, vi: usize, rc: &[f64], rv: &[f64], vpitch: &[f64], rh: f64, z: usize) -> f64 {
+    // vortex line contributions
+    let utw1 = tangential_velocity_vortex_line(rc[ci], rv[vi+1], (vpitch[vi].tan()*rc[vi]/rv[vi+1]).atan(), z);
+    let utw2 = tangential_velocity_vortex_line(rc[ci], rv[vi], (vpitch[vi].tan()*rc[vi]/rv[vi]).atan(), z);
+    // image hub contributions
+    let utwh1 = tangential_velocity_vortex_line(rc[ci], rh*rh/rv[vi+1], (vpitch[vi].tan()*rc[vi]/(rh*rh/rv[vi+1])).atan(), z);
+    let utwh2 = tangential_velocity_vortex_line(rc[ci], rh*rh/rv[vi], (vpitch[vi].tan()*rc[vi]/(rh*rh/rv[vi])).atan(), z);
+    // horsehoe vortex + image hub vortex
+    (utw1 - utwh2) - (utw2 - utwh2)
 }
--- lib/coney/src/lib.rs
+++ lib/coney/src/lib.rs
@@ -1,3 +1,4 @@
+use propeller;
 mod flow;
 
 #[cfg(test)]
